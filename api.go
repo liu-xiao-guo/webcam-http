14c71ea1200c998fe4b773122bc92ca7860762e3
刘晓国 <liu_xiao_guo@yahoo.com>
2015-11-13 14:54:38 +0800

    Changed path to where fswebcam is located!

--- api.go
+++ api.go
@@ -35,7 +35,7 @@ func init() {
 		//}
 		// fmt.Printf("fswebcam is available at %s\n", path)		
 		
-		cmd := exec.Command("/var/lib/apps/webcam.sideload/current/fswebcam", "shot.jpeg")
+		cmd := exec.Command("/apps/webcam.sideload/current/usr/bin/fswebcam", "shot.jpeg")
 		err1 := cmd.Run()
 		fmt.Printf("err1: %d", err1)
 
